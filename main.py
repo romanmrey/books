d5b6805fc74091172556b10e252c73bc7d00ef64
romanmrey <roman.m.rey@gmail.com>
2026-06-24 03:01:40 +0300

    Реализован вывод списка книг и статистика

--- main.py
+++ main.py
@@ -18,6 +18,37 @@ def save_books(books):
     with open(FILENAME, "w", encoding="utf-8") as f:
         json.dump(books, f, ensure_ascii=False, indent=4)
 
+def list_books():
+    books = load_books()
+    if not books:
+        print("Ваш трекер пока пуст.")
+        return
+    print("\n--- Список книг ---")
+    for idx, book in enumerate(books, 1):
+        print(f"{idx}. {book['author']} — «{book['title']}» | Оценка: {book['rating']} | Дата: {book['date_read']}")
+
+
+def show_average_rating():
+    books = load_books()
+    if not books:
+        print("Нет данных для расчета средней оценки.")
+        return
+    avg = sum(book['rating'] for book in books) / len(books)
+    print(f"\nСредняя оценка прочитанных книг: {avg:.2f}")
+
+
+def show_author_stats():
+    books = load_books()
+    if not books:
+        print("Нет данных для статистики.")
+        return
+    stats = {}
+    for book in books:
+        stats[book['author']] = stats.get(book['author'], 0) + 1
+
+    print("\n--- Статистика по авторам ---")
+    for author, count in stats.items():
+        print(f"{author}: прочитано книг — {count}")
 
 def main():
     while True:
@@ -34,11 +65,11 @@ def main():
         if choice == "1":
             print("Функционал в разработке...")
         elif choice == "2":
-            print("Функционал в разработке...")
+            list_books()
         elif choice == "3":
-            print("Функционал в разработке...")
+            show_average_rating()
         elif choice == "4":
-            print("Функционал в разработке...")
+            show_author_stats()
         elif choice == "5":
             print("Функционал в разработке...")
         elif choice == "6":
